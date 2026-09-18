c950b596f94ed751d74db97283e892f3d4aa881f
Aark Bharti <aarkbharti8@gmail.com>
2026-09-18 12:57:26 +0530

    Add initial Express server setup with middleware and API key validation
    - Created package.json for project dependencies and scripts
    - Implemented server.js with Express and Morgan for logging
    - Added API key validation middleware
    - Defined routes for root and data endpoints

---
+++ lecture 16/server.js
@@ -0,0 +1,47 @@
+const express=require("express");
+const morgan=require("morgan");
+const app=express();
+const port=3000;
+
+app.use(morgan());
+
+// const logMiddleware=(req,res,next)=>{
+//     req.name="John Doe";
+//     console.log("Request url:",req.url,"req method:",req.method);
+//     "Time:",new Date().toLocaleString();
+//     // res.send("Hello from middleware");
+//     next();
+// };
+
+const apiCheckMiddleware=(req,res,next)=>{
+    if(req.query.API_KEY==="12345"){
+        console.log("Authenticated");
+        next();
+    }else{
+        res.send("API Invalid")
+    }
+};
+
+app.use(logMiddleware);
+app.use(apiCheckMiddleware);
+
+
+app.get("/",(req,res)=>{
+    console.log("Request name:",req.name);
+    console.log("Hello World");
+    res.send("Hello World");
+});
+
+app.get("/data",(req,res)=>{
+    console.log("Hello Data");
+    res.json({
+        city:"New York",
+        country:"USA"
+        temp:"25C",
+        humidity:"60%"
+    });
+});
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+}); 
